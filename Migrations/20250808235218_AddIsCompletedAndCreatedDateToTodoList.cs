98cd7a8acf58f8188c67e9cb0b4f9d4af1344f31
Abderrahman Abaakil <abderrahmanabaakil@MacBook-Pro-de-Abderrahman.local>
2025-08-09 01:54:06 +0200

    Add IsCompleted and CreatedDate fields to TodoList and update database schema

---
+++ Migrations/20250808235218_AddIsCompletedAndCreatedDateToTodoList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsCompletedAndCreatedDateToTodoList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "TodoLists",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCompleted",
+                table: "TodoLists",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "TodoLists");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "TodoLists");
+        }
+    }
+}
--- Models/TodoList.cs
+++ Models/TodoList.cs
@@ -5,7 +5,11 @@ namespace ToDoList.Models;
 public class TodoList
 {
     public int id { get; set; }
-    
+
     [Required]
-    public required string Content { get; set;  }
+    public required string Content { get; set; }
+     public bool IsCompleted { get; set; } = false;
+        
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    
 }
