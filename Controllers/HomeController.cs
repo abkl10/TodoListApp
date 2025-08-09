db4963705da0fafe5403bb6a0f376724c974f24c
Abderrahman Abaakil <abderrahmanabaakil@MacBook-Pro-de-Abderrahman.local>
2025-08-09 02:21:33 +0200

    Update HomeController to provide dashboard data and add [Authorize] attribute

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Data;
 using ToDoList.Models;
 
 namespace ToDoList.Controllers;
@@ -8,15 +10,24 @@ namespace ToDoList.Controllers;
 [Authorize]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private readonly TodoContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(TodoContext context)
     {
-        _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
+        ViewBag.RecentTasks = await _context.TodoLists
+            .OrderByDescending(t => t.id)
+            .Take(3)
+            .ToListAsync();
+
+        ViewBag.TotalTasks = await _context.TodoLists.CountAsync();
+        ViewBag.CompletedTasks = await _context.TodoLists.CountAsync(t => t.IsCompleted);
+        ViewBag.PendingTasks = await _context.TodoLists.CountAsync(t => !t.IsCompleted);
+
         return View();
     }
 
