27917ddad37e2211d4b0243c885a267f604df27e
Abderrahman Abaakil <abderrahmanabaakil@MacBook-Pro-de-Abderrahman.local>
2025-08-09 02:45:47 +0200

    Add migration to add UserId column to TodoLists table

---
+++ Migrations/20250809004201_AddUserIdToTodoList2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToTodoList2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "TodoLists",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "TodoLists");
+        }
+    }
+}
