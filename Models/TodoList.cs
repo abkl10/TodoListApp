7ef8b1eb01eb5f9626d917cd54b5a0facd0e0b96
Abderrahman Abaakil <abderrahmanebkl48@gmail.com>
2025-08-15 23:26:16 +0200

    Add Category property

--- Models/TodoList.cs
+++ Models/TodoList.cs
@@ -12,5 +12,7 @@ public class TodoList
 
     public DateTime CreatedDate { get; set; } = DateTime.Now;
     public string? UserId { get; set; }
+    public string? Category { get; set; }
+
     
 }
