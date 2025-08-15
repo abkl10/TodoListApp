d024c65e3befae1ac943eacf5e8b09e422e307f7
Abderrahman Abaakil <abderrahmanebkl48@gmail.com>
2025-08-15 23:28:36 +0200

    Add EF migration

---
+++ Migrations/20250815212715_AddCategoryToTodo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToTodo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "TodoLists",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "TodoLists");
+        }
+    }
+}
