df5b21f7c42ac295e1236f618731809e24dcee29
Abderrahman Abaakil <abderrahmanebkl48@gmail.com>
2025-08-29 22:50:35 +0200

    dd AdminController with dashboard

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Controllers;
+
+[Authorize(Roles = "Admin")]
+public class AdminController : Controller
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly TodoContext _context;
+
+    public AdminController(UserManager<IdentityUser> userManager, TodoContext context)
+    {
+        _userManager = userManager;
+        _context = context;
+    }
+
+    public async Task<IActionResult> Dashboard()
+    {
+        var users = await _userManager.Users.ToListAsync();
+        ViewBag.UserCount = users.Count;
+        ViewBag.TasksCount = await _context.TodoLists.CountAsync();
+        return View(users);
+    }
+
+    
+}
